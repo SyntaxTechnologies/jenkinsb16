7f0c41d17cc9b9d9bcc2bc003372b729a8fc2fbb
moazzamsadiq <moazzam@syntaxtechs.com>
2023-08-09 20:20:20 -0400

    sfsf

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -53,7 +53,7 @@ public class LoginSteps extends CommonMethods {
 
     @Then("user is successfully logged in the application")
     public void user_is_successfully_logged_in_the_application() {
-        System.out.println("heelo");
+        Assert.assertTrue(false);
     }
 
     @When("user enters ess username and password")
